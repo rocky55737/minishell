edaf4bd4b2b91eeee8c9734e55205970bdad8dd3
RHONG <rocky55737@gmail.com>
2022-11-25 15:22:59 +0900

    sig test:

--- testfiles/test.c
+++ testfiles/test.c
@@ -15,17 +15,17 @@ void	sigint_handler(int	sig);
 int main(void)
 {
 	//readline stat test
-	char	*buff;
-	struct stat struct_stat;
-	do
-	{
-		buff = readline("minishell $");
-		if (stat(buff, &struct_stat) == -1)
-			strerror(errno);
-		printf("%d\n", (int)struct_stat.st_mode);
-		if ((struct_stat.st_mode & 0170000) == 0040000)
-			printf("isdir\n");
-	} while (buff);
+	//char	*buff;
+	//struct stat struct_stat;
+	//do
+	//{
+	//	buff = readline("minishell $");
+	//	if (stat(buff, &struct_stat) == -1)
+	//		strerror(errno);
+	//	printf("%d\n", (int)struct_stat.st_mode);
+	//	if ((struct_stat.st_mode & 0170000) == 0040000)
+	//		printf("isdir\n");
+	//} while (buff);
 
 	//isatty test
 	// char	*buff;
@@ -49,31 +49,56 @@ int main(void)
 	// while (1)
 	// {}
 
-	// pid_t	pid;
-	// int		status;
-	// struct rusage s_rusage;
+	// signal test
+	pid_t	pid;
+	int		status;
 
-	// pid = fork();
-	// signal(2, sigint_handler);
-	// if (pid == 0)
-	// {
-	// 	while (1)
-	// 	{
-	// 	}
-	// }
-	// else
-	// {
-	// 	system("sleep 5\n");
-	// 	kill(pid, 3);
-	// 	wait4(pid, &status, 0, &s_rusage);
-	// 	printf("usag: %d\n", getrusage());
-	// }
+	pid = fork();
+	if (pid == 0)
+	{
+		signal(2, multi_sig_handler_child);
+		signal(3, multi_sig_handler_child);
+		signal(15, multi_sig_handler_child);
+		while (1)
+		{
+		}
+	}
+	else
+	{
+		signal(2, parent);
+		waitpid(pid, &status, 0);
+		exit(status >> 8);
+	}
 	return (0);
 }
 
 //signal function
-// void	sigint_handler(int	sig)
-// {
-// 	write(2, "the sigint called!\n", strlen("the sigint called!\n"));
-// 	exit(1);
-// }
+//SIGINT: 2: Ctrl + C: interept process and carrage return
+//SIGQUIT: 3: Ctrl + \: exit shell
+//SIGTERM: 15: Ctrl + D: ignore
+void	sigint_handler(int	sig)
+{
+	printf("\b\bSIGINT!\n");
+	exit(1);
+}
+
+void	sigquit_handler(int	sig)
+{
+	printf("SIGQUIT!\n");
+	exit(130);
+}
+
+void	sigterm_handler(int	sig)
+{
+	printf("\b\bSIGTERM!\n");
+}
+
+void	multi_sig_handler_child(int sig_n)
+{
+	if (sig_n == 2)
+		sigint_handler(sig_n);
+	else if (sig_n == 3)
+		sigquit_handler(sig_n);
+	else if (sig_n == 15)
+		sigterm_handler(sig_n);
+}
